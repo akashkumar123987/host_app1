8da41173f7508b84138b9e3dfe79e1947135ada3
Akash Kumar <ak9608450671@gmail.com>
2024-07-11 22:15:17 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,13 +9,13 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors(
-    {
-        origin:["https://deploy-mern-1whq.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-));
+app.use(cors());
+    // {
+    //     origin:["https://deploy-mern-1whq.vercel.app"],
+    //     methods:["POST","GET"],
+    //     credentials:true
+    // }
+
 app.use(express.json());
 
 dotenv.config();
